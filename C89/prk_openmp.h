44fe16eef4ac1798af1ba84409d0f742bb1c6844
Jeff Hammond <jeff.science@gmail.com>
2016-02-24 02:32:46 -0600

    bail_out fixup and delete unnecessary defs files

--- C89/prk_openmp.h
+++ C89/prk_openmp.h
@@ -39,6 +39,28 @@ POSSIBILITY OF SUCH DAMAGE.
 #define MAX_THREADS MAXTHREADS
 #endif
 
+/**********************************************************************
+
+Name:      bail_out
+
+Purpose:   Exit gracefully when an OpenMP thread has encountered an error
+           inside a parallel region
+
+Arguments: error code (zero for no error).
+
+Returns:   nothing, but the program terminates with a nonzero exit status
+
+Notes:     This function must be called by all threads in the team. Multiple
+           threads may have tried to update the shared error variable at the
+           same time, so this needs to be done atomically if we want to
+           guarantee that the value of 1 is put into error. In our case,
+           however, we merely want to know if the value is different from
+           zero, so we do not need atomicity.
+
+History:   Written by Rob Van der Wijngaart, July 2006
+
+**********************************************************************/
+
 #if defined(_OPENMP)
 static void omp_bail_out(int error)
 {
