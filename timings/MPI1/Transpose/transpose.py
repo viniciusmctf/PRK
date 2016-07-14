2365a346e663ba8d2d5bb034493cba2986807224
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2016-07-14 15:08:39 -0700

    Clean up naming scheme

--- timings/MPI1/Transpose/transpose.py
+++ timings/MPI1/Transpose/transpose.py
@@ -4,8 +4,8 @@ template="""#!/bin/bash -l
 #SBATCH -p debug
 #SBATCH -N %d
 #SBATCH -t 00:03:00
-#SBATCH -J transpose_mpi_%d
-#SBATCH -o transpose_mpi_%d.out
+#SBATCH -J transpose_mpi_%04d
+#SBATCH -o transpose_mpi_%04d.out
 
 # Run 25 iterations
 date
@@ -17,7 +17,7 @@ nnodes=[1,2,4,8,16,32,64]
 procpernode=24 # On Edison
 
 for inode in nnodes :
-  fn = "transpose_%d.sh"%inode
+  fn = "transpose_%04d.sh"%inode
   comm=template%(inode,inode,inode,inode*procpernode)
   ff = open(fn,"w")
   ff.write(comm)
